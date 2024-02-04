c3b6d52d8d93bb7b128d328b3af21d74aab2a257
Peter Szathmary <peter.szathmary01@gmail.com>
2024-02-04 09:48:22 +0100

    back-propagation fixed for 3 layer neural network

--- src/main/java/szathmary/peter/neuralnetwork/trainingalgorithms/BackPropagation.java
+++ src/main/java/szathmary/peter/neuralnetwork/trainingalgorithms/BackPropagation.java
@@ -16,51 +16,89 @@ public class BackPropagation extends TrainingAlgorithm {
   }
 
   @Override
-  protected void backPropagate(NeuralNetwork neuralNetwork, double error) {
+  protected void backPropagate(NeuralNetwork neuralNetwork, double[] error) {
     if (neuralNetwork == null) {
       throw new IllegalArgumentException("Cannot use back-propagation on null neuralNetwork!");
     }
 
     if (neuralNetwork.getNumberOfLayers() != 3) {
       throw new IllegalStateException(
-          "Back propagation algorithm not implemented for neural neuralNetwork with more than 3 layers!");
+          "Back propagation algorithm not implemented for neural networks with more than 3 layers!");
     }
 
     calculateWeightsForUpperLayer(neuralNetwork, error);
     calculateWeightsForBottomLayer(neuralNetwork, error);
   }
 
-  private void calculateWeightsForUpperLayer(NeuralNetwork neuralNetwork, double error) {
+  private void calculateWeightsForUpperLayer(NeuralNetwork neuralNetwork, double[] errors) {
     Layer outputLayer = neuralNetwork.getOutputLayer();
-    Layer hiddenLayer = neuralNetwork.getHiddenLayer(0);
+    Layer lastHiddenLayer =
+        neuralNetwork.getHiddenLayer(neuralNetwork.getNumberOfHiddenLayers() - 1);
 
-    Neuron outputNeuron = outputLayer.getNeuron(0);
+    for (int i = 0; i < outputLayer.getNeuronCount(); i++) {
+      Neuron outputNeuron = outputLayer.getNeuron(i);
 
-    for (int i = 0; i < outputNeuron.getWeights().length; i++) {
-      double derivative =
-          outputNeuron.getActivationFunction().applyForDerivation(outputNeuron.getWeightedInput());
-      double gradient = -error * derivative * hiddenLayer.getOutputs()[i];
-      double updatedWeight = outputNeuron.getWeights()[i] - learningRate * gradient;
-      outputNeuron.setWeight(updatedWeight, i);
+      for (int j = 0; j < outputNeuron.getWeights().length; j++) {
+        double derivative =
+            outputNeuron
+                .getActivationFunction()
+                .applyForDerivation(outputNeuron.getWeightedInput());
+        double gradient = -errors[i] * derivative * lastHiddenLayer.getOutputs()[j];
+        double updatedWeight = outputNeuron.getWeights()[j] - learningRate * gradient;
+        outputNeuron.setWeight(updatedWeight, j);
+      }
     }
   }
 
-  private void calculateWeightsForBottomLayer(NeuralNetwork neuralNetwork, double error) {
-    Layer hiddenLayer = neuralNetwork.getHiddenLayer(0);
-    Layer outputLayer = neuralNetwork.getOutputLayer();
-    Neuron outputNeuron = outputLayer.getNeuron(0);
+  //  private void calculateWeightsForBottomLayer(NeuralNetwork neuralNetwork, double[] errors) {
+  //    Layer firstHiddenLayer = neuralNetwork.getHiddenLayer(0);
+  //    Layer outputLayer = neuralNetwork.getOutputLayer();
+  //
+  //    for (Neuron hiddenNeuron : firstHiddenLayer.getNeuronList()) {
+  //      double totalError = 0;
+  //      for (int i = 0; i < outputLayer.getNeuronCount(); i++) {
+  //        Neuron outputNeuron = outputLayer.getNeuron(i);
+  //        double outputNeuronWeight =
+  // outputNeuron.getWeights()[firstHiddenLayer.getNeuronList().indexOf(hiddenNeuron)];
+  //        double derivative =
+  // hiddenNeuron.getActivationFunction().applyForDerivation(hiddenNeuron.getWeightedInput());
+  //        totalError += outputNeuronWeight * derivative * errors[i];
+  //      }
+  //      for (int j = 0; j < hiddenNeuron.getWeights().length; j++) {
+  //        double inputVal = hiddenNeuron.getInputs()[j];
+  //        double gradient = -totalError * inputVal;
+  //        double updatedWeight = hiddenNeuron.getWeights()[j] - learningRate * gradient;
+  //        hiddenNeuron.setWeight(updatedWeight, j);
+  //      }
+  //    }
+  //  }
 
-    for (Neuron hiddenNeuron : hiddenLayer.getNeuronList()) {
-      double outputNeuronWeight =
-          outputNeuron.getWeights()[hiddenLayer.getNeuronList().indexOf(hiddenNeuron)];
-      double derivative =
+  private void calculateWeightsForBottomLayer(NeuralNetwork neuralNetwork, double[] errors) {
+    Layer firstHiddenLayer = neuralNetwork.getHiddenLayer(0);
+    Layer outputLayer = neuralNetwork.getOutputLayer();
+
+    for (int hiddenIndex = 0; hiddenIndex < firstHiddenLayer.getNeuronCount(); hiddenIndex++) {
+      Neuron hiddenNeuron = firstHiddenLayer.getNeuron(hiddenIndex);
+      double totalError = 0;
+
+      for (int outputIndex = 0; outputIndex < outputLayer.getNeuronCount(); outputIndex++) {
+        Neuron outputNeuron = outputLayer.getNeuron(outputIndex);
+        double outputNeuronWeight = outputNeuron.getWeights()[hiddenIndex];
+        double derivative =
+            outputNeuron
+                .getActivationFunction()
+                .applyForDerivation(outputNeuron.getWeightedInput());
+        totalError += outputNeuronWeight * derivative * errors[outputIndex];
+      }
+
+      double hiddenDerivative =
           hiddenNeuron.getActivationFunction().applyForDerivation(hiddenNeuron.getWeightedInput());
-      double propagatedError = outputNeuronWeight * derivative * error;
-      for (int i = 0; i < hiddenNeuron.getWeights().length; i++) {
-        double inputVal = hiddenNeuron.getInputs()[i];
-        double gradient = -propagatedError * inputVal;
-        double updatedWeight = hiddenNeuron.getWeights()[i] - learningRate * gradient;
-        hiddenNeuron.setWeight(updatedWeight, i);
+
+      for (int inputIndex = 0; inputIndex < hiddenNeuron.getWeights().length; inputIndex++) {
+        double inputVal = hiddenNeuron.getInputs()[inputIndex];
+        double gradient = -totalError * hiddenDerivative * inputVal;
+        double updatedWeight = hiddenNeuron.getWeights()[inputIndex] - learningRate * gradient;
+        hiddenNeuron.setWeight(updatedWeight, inputIndex);
       }
     }
   }
